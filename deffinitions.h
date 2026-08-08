14d5ae6f7ae0e1f930a8447df97c007fe3a10037
youdontknowmeaf <core.contact@yahoo.com>
2026-08-08 18:45:42 +0200

    Cleaned src code

---
+++ deffinitions.h
@@ -0,0 +1,23 @@
+//
+// Created by fuckingfuck81 on 8/8/26.
+//
+
+#ifndef DEFFINITIONS_H
+#define DEFFINITIONS_H
+
+#define BAD_UPDATE 1
+#define BAD_AVATAR 2
+#define NONE 0
+#define TONY_HAWK 1
+#define ROCK_BAND 2
+#define INSTALL_BAD_UPDATE 1
+#define INSTALL_BAD_AVATAR 2
+#define CONVERT_GAME_BY_PATH 3
+#define INSTALL_ROCKBAND 11
+#define INSTALL_TONYHAWK 12
+#define INSTALL_PAYLOAD 1
+#define FORMAT_ISO 2
+#define NEWEST -1
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -8,20 +8,6 @@
 #include <atomic>
 #include <cmath>
 
-#define BAD_UPDATE 1
-#define BAD_AVATAR 2
-#define NONE 0
-#define TONY_HAWK 1
-#define ROCK_BAND 2
-#define INSTALL_BAD_UPDATE 1
-#define INSTALL_BAD_AVATAR 2
-#define CONVERT_GAME_BY_PATH 3
-#define INSTALL_ROCKBAND 11
-#define INSTALL_TONYHAWK 12
-#define INSTALL_PAYLOAD 1
-#define FORMAT_ISO 2
-#define NEWEST -1
-
 // That func is not written by me :-p
 std::atomic<bool> is_working(false);
 void run_async(const std::string& command, std::atomic<bool>& status_flag) {
